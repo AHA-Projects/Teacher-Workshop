0d07f7de3ce2f3e50fe6663218ccdb67793d79c1
Anany Sharma <41077586+ananyd36@users.noreply.github.com>
2025-09-18 18:55:26 -0400

    Update water_leak_detection.ino
    import library changed as per template name

--- Session_2_Activities/water_leak_detection/water_leak_detection.ino
+++ Session_2_Activities/water_leak_detection/water_leak_detection.ino
@@ -13,7 +13,7 @@
 // It uses a light sensor to analyze the color of a surface and an OLED screen to display if a leak is detected.
 
 // Importing the Edge Impulse library, which contains the pre-trained model for water leak detection.
-#include <Water_Leak_Detection_inferencing.h>
+#include <Water_Leak_Detection_Template_inferencing.h>
 
 // Importing the library for the APDS9960 sensor, a versatile sensor that can detect ambient light and color.
 #include "Adafruit_APDS9960.h"
